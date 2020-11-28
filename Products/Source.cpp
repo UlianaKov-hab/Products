7b0fe419784c1ca798f0a2eebff1caf6aac9f833
UlianaKov-hab <a_k_ula@ukr.net>
2020-11-28 22:33:22 +0200

    Add array newProduct

--- Products/Source.cpp
+++ Products/Source.cpp
@@ -3,18 +3,37 @@
 int main()
 {
 	srand(time(0));
-	Product newProduct;
+	int size;
+	cout << "Enter amount of products ";
+	cin >> size;
+	//Product newProduct;
+	Product* newProduct = new Product[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		newProduct[i].setNameRandomly();
+		newProduct[i].setDiscription();
+		newProduct[i].setStarting_priceFromKey();
+		newProduct[i].setDiscountFromKey();
+		newProduct[i].setGeneral_discount();
+		newProduct[i].setResidual_value();
+		system("CLS");
+	}
 
 	//newProduct.setNameFromKey();
-	newProduct.setNameRandomly();
+	/*newProduct.setNameRandomly();
 	newProduct.setDiscription();
 	newProduct.setStarting_priceFromKey();
 	newProduct.setDiscountFromKey();
 	newProduct.setGeneral_discount();
-	newProduct.setResidual_value();
+	newProduct.setResidual_value();*/
 	system("CLS");
-	newProduct.printProduct();
+	for (size_t i = 0; i < size; i++)
+	{
+		newProduct[i].printProduct();
+		cout << "____________________________________" << endl;
+	}
+	//newProduct[0].printProduct();
 
 	
-	
+	delete[]newProduct;
 }
